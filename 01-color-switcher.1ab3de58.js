a1fe6eaab246f7ba8045f3177d47016a0b496deb
Bohatynska <98977553+Bohatynska@users.noreply.github.com>
2022-10-29 21:40:49 +0000

    Deploying to gh-pages from @ Bohatynska/goit-js-hw-09@f5886a31badb51d496c0efada2d554f820fb30da 🚀

---
+++ 01-color-switcher.1ab3de58.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));var n=null}();
+//# sourceMappingURL=01-color-switcher.1ab3de58.js.map
