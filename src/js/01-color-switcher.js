f5886a31badb51d496c0efada2d554f820fb30da
Bohatynska Oksana <98977553+Bohatynska@users.noreply.github.com>
2022-10-29 22:40:27 +0100

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStartRef = document.querySelector('button[data-start]');
+const btnStopRef = document.querySelector('button[data-stop]');
+
+btnStartRef.addEventListener('click', onStartClick);
+btnStopRef.addEventListener('click', onstopClick);
+
+let intervalId = null;
+
+function onStartClick() {
+  //   btnStartRef.disabled = true;
+  btnStartRef.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onstopClick() {
+  btnStartRef.disabled = false;
+  //   btnStopRef.removeAttribute('disable');
+  clearInterval(intervalId);
+}
